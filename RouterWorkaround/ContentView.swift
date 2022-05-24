4e8c6e307cf94129a491a53b68f0150f7c440882
Danil Lomaev <d.lomaev@vk.team>
2022-05-24 11:31:26 +0300

    Initial Commit

---
+++ RouterWorkaround/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RouterWorkaround
+//
+//  Created by Данил Ломаев on 24.05.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RouterWorkaround/RouterWorkaroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  RouterWorkaroundApp.swift
+//  RouterWorkaround
+//
+//  Created by Данил Ломаев on 24.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct RouterWorkaroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
